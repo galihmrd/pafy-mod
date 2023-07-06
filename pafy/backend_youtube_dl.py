4baaac24b42a135e28d0d122c3ec74b8e6768c0f
galihmrd <galihmardiana15@gmail.com>
2023-07-06 11:01:45 +0700

    remove unused line

--- pafy/backend_youtube_dl.py
+++ pafy/backend_youtube_dl.py
@@ -117,7 +117,6 @@ class YtdlStream(BaseStream):
             self._mediatype = "normal"
 
         self._threed = info.get("format_note") == "3D"
-        self._rawbitrate = info.get("abr", 0) * 1024
 
         height = info.get("height") or 0
         width = info.get("width") or 0
